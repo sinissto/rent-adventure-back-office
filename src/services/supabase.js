3269a27d6dba858bb02ff0fea0862664c7267218
Sinisa <sinisha.stojanovic@gmail.com>
2025-01-22 10:10:03 +0100

    Supabase package installed and initialized

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hwomlhbltvjvyjobrsns.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh3b21saGJsdHZqdnlqb2Jyc25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2ODM2NzMsImV4cCI6MjA1MDI1OTY3M30.EHUkhqATbxRZhLC7Hr0J49na9t-cobN3sVLLpLJcuOU";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
