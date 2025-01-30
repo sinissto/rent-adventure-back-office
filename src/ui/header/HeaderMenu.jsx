39f60b324c4b51fb9172b2d308e4f2019f11ef06
Sinisa <sinisha.stojanovic@gmail.com>
2025-01-30 21:39:04 +0100

    Main app Header implemented

---
+++ src/ui/header/HeaderMenu.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import ButtonIcon from "./ButtonIcon.jsx";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router";
+import Logout from "../../features/authentication/Logout.jsx";
+
+const StyledHeaderManu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderManu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <Logout />
+    </StyledHeaderManu>
+  );
+}
+
+export default HeaderMenu;
